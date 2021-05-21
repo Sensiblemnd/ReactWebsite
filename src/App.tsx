a0f7ddf03b76ca2f4dbd96e19236eddb6ec21f35
Rob Lloveras <Sensiblemnd@users.noreply.github.com>
2021-05-20 23:38:45 -0400

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,7 @@ import AppWrapper from "./containers/app-wrapper";
 function App() {
   return (
     <AppWrapper>
-      <div className="App">
-        <div className="side-menu">Side Menu</div>
-        <div className="main-resizable">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-          accusantium dicta vitae illum rerum, repellendus veritatis quisquam
-          dolorum quod! Tempore molestias asperiores totam quibusdam beatae
-          minima aperiam a, reprehenderit culpa!
-        </div>
-      </div>
+      <div className="App"></div>
     </AppWrapper>
   );
 }
